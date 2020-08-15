c0208123cd4596a8a4d701d77293e32e7411bd35
alpakih <ahmaddikri13@gmail.com>
2020-08-15 21:02:20 +0700

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-container>
+      <h4>Created By</h4>
+      <v-list-item>
+          <v-list-item-avatar height="100px" width="100px">
+              <v-img src="img/ahmad.png" ></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+              <v-list-item-title>Nama</v-list-item-title>
+              <v-list-item-subtitle>Ahmad Dikri Alpakih</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Tempat, Tanggal Lahir</v-list-item-title>
+              <v-list-item-subtitle>Bandung, 11 Juli 1997</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Alamat</v-list-item-title>
+              <v-list-item-subtitle>Jl. Babakan Ciparay, Bandung</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Hobi</v-list-item-title>
+              <v-list-item-subtitle>Berenang</v-list-item-subtitle>
+          </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+          <v-list-item-content>
+              <v-list-item-title>Nama</v-list-item-title>
+              <v-list-item-subtitle>Pipit Fitriyani</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Tempat, Tanggal Lahir</v-list-item-title>
+              <v-list-item-subtitle>Bandung, 21 April 1999</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Alamat</v-list-item-title>
+              <v-list-item-subtitle>Kp Hegarmanah</v-list-item-subtitle>
+              <v-divider></v-divider>
+              <v-list-item-title>Hobi</v-list-item-title>
+              <v-list-item-subtitle>Olahraga</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-avatar height="100px" width="100px">
+              <v-img src="img/pipit.png" ></v-img>
+          </v-list-item-avatar>
+      </v-list-item>
+    </v-container>
+  </div>
+</template>
